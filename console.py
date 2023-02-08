fd624246940f3f35e0514f27885a757833fecbbe
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-02-08 09:21:41 +0100

    add do_create in console.py

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the console.py module with class HBNBCommand"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -23,5 +24,18 @@ class HBNBCommand(cmd.Cmd):
         """returns nothing for emptyline + enter"""
         pass
 
+    def do_create(self, class_name):
+        "Creates a new BaseModel instance, saves and prints the ID"
+        if class_name:
+            if type(class_name) is BaseModel:
+                my_model = BaseModel()
+                my_model.save()
+                print(my_model.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+
 if __name__ == "__main__":
-        HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
