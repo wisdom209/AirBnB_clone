76206a33de5d1aae21fcb31b8b61ba887ce3fcd6
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-02-08 19:01:03 +0100

    do_create for all classes done

--- console.py
+++ console.py
@@ -13,172 +13,174 @@ from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
-	"""
-	class definition for HBNBCommand that inherits from Cmd class for
-	to build line oriented command interpreter
-	"""
+    """
+    class definition for HBNBCommand that inherits from Cmd class for
+    to build line oriented command interpreter
+    """
 
-	prompt = "(hbnb) "
-	clasname = ("BaseModel", "User", "City", "State",
-				"Amenity", "Place", "Review")
+    prompt = "(hbnb) "
 
-	def do_quit(self, line):
-		"""quit command to exit from the interprter"""
-		return True
+    class_tuple = ("BaseModel", "User", "City", "State",
+                   "Amenity", "Place", "Review")
 
-	def do_EOF(self, line):
-		"""ctrl+D use to kill the program and exit from the interpreter"""
-		return True
+    def do_quit(self, line):
+        """quit command to exit from the interprter"""
+        return True
 
-	def emptyline(self):
-		"""returns nothing for emptyline + enter"""
-		pass
+    def do_EOF(self, line):
+        """ctrl+D use to kill the program and exit from the interpreter"""
+        return True
 
-	def do_create(self, class_name):
-		"Creates a new BaseModel instance, saves and prints the ID"
-		if class_name:
-			if class_name == "BaseModel":
-				my_model = BaseModel()
-				my_model.save()
-				print(my_model.id)
-			else:
-				print("** class doesn't exist **")
-		else:
-			print("** class name missing **")
+    def emptyline(self):
+        """returns nothing for emptyline + enter"""
+        pass
 
-	def do_show(self, line):
-		"""Prints the string representation of an instance based on the class name and id. Ex: $ show BaseModel 1234-1234-1234."""
-		class_name = None
-		instance_id = None
-		args = None
+    def do_create(self, class_name):
+        "Creates a new BaseModel instance, saves and prints the ID"
 
-		if (line and len(line) != 0):
-			args = line.split()
-			class_name = args[0]
+        if class_name:
+            if class_name in self.class_tuple:
+                my_model = eval(class_name + "()")
+                my_model.save()
+                print(my_model.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
-			if (len(args) > 1):
-				instance_id = args[1]
+    def do_show(self, line):
+        """Prints the string representation of an instance based on the class name and id. Ex: $ show BaseModel 1234-1234-1234."""
+        class_name = None
+        instance_id = None
+        args = None
 
-		if class_name:
-			if class_name != "BaseModel":
-				print("** class doesn't exist **")
-			else:
-				if not instance_id:
-					print("** instance id missing **")
+        if (line and len(line) != 0):
+            args = line.split()
+            class_name = args[0]
 
-				if instance_id:
-					obj = FileStorage()
-					full_key = f"BaseModel.{instance_id}"
+            if (len(args) > 1):
+                instance_id = args[1]
 
-					if full_key in obj.all().keys():
-						new_obj_dict = copy.deepcopy(obj.all())
-						for value in new_obj_dict.values():
-							if '__class__' in value.__dict__.keys():
-								del value.__dict__['__class__']
-						print(new_obj_dict[full_key])
-					else:
-						print("** no instance found **")
-		else:
-			print("** class name missing **")
+        if class_name:
+            if class_name != "BaseModel":
+                print("** class doesn't exist **")
+            else:
+                if not instance_id:
+                    print("** instance id missing **")
 
-	def do_destroy(self, line):
-		"""Deletes an instance based on the class name and id (save the change into the JSON file). Ex: $ destroy BaseModel 1234-1234-1234."""
-		class_name = None
-		instance_id = None
-		args = None
+                if instance_id:
+                    obj = FileStorage()
+                    full_key = f"BaseModel.{instance_id}"
 
-		if (line and len(line) != 0):
-			args = line.split()
-			class_name = args[0]
+                    if full_key in obj.all().keys():
+                        new_obj_dict = copy.deepcopy(obj.all())
+                        for value in new_obj_dict.values():
+                            if '__class__' in value.__dict__.keys():
+                                del value.__dict__['__class__']
+                        print(new_obj_dict[full_key])
+                    else:
+                        print("** no instance found **")
+        else:
+            print("** class name missing **")
 
-			if (len(args) > 1):
-				instance_id = args[1]
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id (save the change into the JSON file). Ex: $ destroy BaseModel 1234-1234-1234."""
+        class_name = None
+        instance_id = None
+        args = None
 
-		if class_name:
-			if class_name != "BaseModel":
-				print("** class doesn't exist **")
-			else:
-				if not instance_id:
-					print("** instance id missing **")
+        if (line and len(line) != 0):
+            args = line.split()
+            class_name = args[0]
 
-				if instance_id:
-					obj = FileStorage()
-					full_key = f"BaseModel.{instance_id}"
+            if (len(args) > 1):
+                instance_id = args[1]
 
-					if full_key in obj.all().keys():
-						del obj.all()[full_key]
-						f = FileStorage()
-						f.save()
-					else:
-						print("** no instance found **")
-		else:
-			print("** class name missing **")
+        if class_name:
+            if class_name != "BaseModel":
+                print("** class doesn't exist **")
+            else:
+                if not instance_id:
+                    print("** instance id missing **")
 
-	def do_all(self, class_name):
-		"""Prints all string representation of all instances based or not on the class name. Ex: $ all BaseModel or $ all."""
-		if not class_name or class_name == "BaseModel":
-			obj = FileStorage()
-			new_obj_dict = copy.deepcopy(obj.all())
-			for value in new_obj_dict.values():
-				if '__class__' in value.__dict__.keys():
-					del value.__dict__['__class__']
-			obj_dict = [str(x) for x in new_obj_dict.values()]
-			print(obj_dict)
-		else:
-			print("** class doesn't exist **")
+                if instance_id:
+                    obj = FileStorage()
+                    full_key = f"BaseModel.{instance_id}"
 
-	def do_update(self, line):
-		"""Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file). Ex: $ update BaseModel 1234-1234-1234 email 'aibnb@mail.com'."""
-		class_name = None
-		id = None
-		attr_name = None
-		attr_value = None
-		args_used = [class_name, id, attr_name, attr_value]
-		args = line.split()
-		tot_args = len(args)
+                    if full_key in obj.all().keys():
+                        del obj.all()[full_key]
+                        f = FileStorage()
+                        f.save()
+                    else:
+                        print("** no instance found **")
+        else:
+            print("** class name missing **")
 
-		for i in range(tot_args):
-			if i == 0:
-				class_name = args[0]
-			if i == 1:
-				id = args[1]
-			if i == 2:
-				attr_name = args[2]
-			if i == 3:
-				attr_value = args[3]
+    def do_all(self, class_name):
+        """Prints all string representation of all instances based or not on the class name. Ex: $ all BaseModel or $ all."""
+        if not class_name or class_name == "BaseModel":
+            obj = FileStorage()
+            new_obj_dict = copy.deepcopy(obj.all())
+            for value in new_obj_dict.values():
+                if '__class__' in value.__dict__.keys():
+                    del value.__dict__['__class__']
+            obj_dict = [str(x) for x in new_obj_dict.values()]
+            print(obj_dict)
+        else:
+            print("** class doesn't exist **")
 
-		if class_name:
-			if class_name != "BaseModel":
-				print("*** class doesn't exist ***")
-			else:
-				if id:
-					obj = FileStorage()
-					full_key = f"BaseModel.{id}"
+    def do_update(self, line):
+        """Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file). Ex: $ update BaseModel 1234-1234-1234 email 'aibnb@mail.com'."""
+        class_name = None
+        id = None
+        attr_name = None
+        attr_value = None
+        args_used = [class_name, id, attr_name, attr_value]
+        args = line.split()
+        tot_args = len(args)
 
-					if full_key in obj.all().keys():
-						if not attr_name:
-							print("** attribute name is missing **")
-						else:
-							if not attr_value:
-								print("** value missing **")
-							else:
-								obj = FileStorage()
-								obj_to_update = obj.all()[full_key]
-								add_val = attr_value
-								try:
-									add_val = eval(attr_value)
-								except Exception:
-									pass
-								obj_to_update.__dict__[
-									attr_name] = add_val
-								obj.save()
-					else:
-						print("** no instance found **")
-				else:
-					print("** instance id missing **")
-		else:
-			print("*** class name is missing ***")
+        for i in range(tot_args):
+            if i == 0:
+                class_name = args[0]
+            if i == 1:
+                id = args[1]
+            if i == 2:
+                attr_name = args[2]
+            if i == 3:
+                attr_value = args[3]
+
+        if class_name:
+            if class_name != "BaseModel":
+                print("*** class doesn't exist ***")
+            else:
+                if id:
+                    obj = FileStorage()
+                    full_key = f"BaseModel.{id}"
+
+                    if full_key in obj.all().keys():
+                        if not attr_name:
+                            print("** attribute name is missing **")
+                        else:
+                            if not attr_value:
+                                print("** value missing **")
+                            else:
+                                obj = FileStorage()
+                                obj_to_update = obj.all()[full_key]
+                                add_val = attr_value
+                                try:
+                                    add_val = eval(attr_value)
+                                except Exception:
+                                    pass
+                                obj_to_update.__dict__[
+                                    attr_name] = add_val
+                                obj.save()
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+        else:
+            print("*** class name is missing ***")
 
 
 if __name__ == "__main__":
-	HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
