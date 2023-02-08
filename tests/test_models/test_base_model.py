720043fa8924f8502826acfef34f99381970c6ab
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-02-08 20:33:16 +0100

    fix pycodestyle

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,11 +3,13 @@
 from models.base_model import BaseModel
 import unittest
 
+
 class Test_BaseModel(unittest.TestCase):
     '''Test BaseModel class'''
+
     def test_class_init(self):
         b = BaseModel()
         b.name = "me"
-        b.number= 98
+        b.number = 98
         self.assertEqual(b.name, "me")
         self.assertEqual(b.number, 98)
