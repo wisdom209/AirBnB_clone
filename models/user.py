5a87f728da98373d538a52c5db5660bc5edaa7bf
Tessyjames28 <tessyjames28@gmail.com>
2023-02-12 08:24:13 +0100

    user class

--- models/user.py
+++ models/user.py
@@ -9,7 +9,7 @@ class User(BaseModel):
     assigns public class attributes that defines users
 
     Arguments:
-            emails (str) - empty string
+            email (str) - empty string
             password (str) - empty string
             first_name (str) - empty string
             last_name (str) - empty string
