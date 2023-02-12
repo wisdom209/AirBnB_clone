c2cea425943bab4833dbf5f0c1ecec61b69c6544
Tessyjames28 <tessyjames28@gmail.com>
2023-02-12 08:44:02 +0100

    documentation formatting

--- models/city.py
+++ models/city.py
@@ -9,8 +9,8 @@ class City(BaseModel):
     inherits from BaseModel and defines City class attributes
 
     Arguments:
-                state_id (str) - empty string: it will be the State.id
-                name (str) - empty string
+        state_id (str) - empty string: it will be the State.id
+        name (str) - empty string
     """
 
     state_id = ""
