6d3798ca0d5b8b846ec3625151334e6668e9c6d3
Tessyjames28 <tessyjames28@gmail.com>
2023-02-12 08:45:13 +0100

    dcumentatin formatting

--- models/review.py
+++ models/review.py
@@ -9,9 +9,9 @@ class Review(BaseModel):
     inherits from BaseModel and defines the Review class attributes
 
     Arguments:
-                    place_id (str) - empty string: it will be the Place.id
-                    user_id (str) - empty string: it will be the User.id
-                    text (str) - empty string
+        place_id (str) - empty string: it will be the Place.id
+        user_id (str) - empty string: it will be the User.id
+        text (str) - empty string
     """
 
     place_id = ""
